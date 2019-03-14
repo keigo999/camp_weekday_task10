98732427055aa40581151732f0dc2921fd037240
keigo999 <highbridgek5@gmail.com>
2019-03-14 13:07:01 +0900

    登録画面

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -2,20 +2,37 @@ class AreasController < ApplicationController
   def index
   end
 
-  def new
+  def search_get
     @area = Area.new
   end
 
   def search
-    @area = Area.new
-    uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?zipcode=#{@area}")
-    @area2 = Net::HTTP.get_response(uri)
-    redirect_to 
+    uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?zipcode=#{params[:zipcode]}")
+    response = Net::HTTP.get_response(uri)
+    if response
+      @result = JSON.parse(response.body)
+      @zipcode = @result["results"][0]["zipcode"]
+      @prefcode = @result["results"][0]["prefcode"]
+      @address1 = @result["results"][0]["address1"]
+      @address2 = @result["results"][0]["address2"]
+      @address3 = @result["results"][0]["address3"]
+      @kana1 = @result["results"][0]["kana1"]
+      @kana2 = @result["results"][0]["kana2"]
+      @kana3 = @result["results"][0]["kana3"]
+      render "new"
+    end
+  end
+
+  def create
+    @area = Area.new(area_params)
+    if @area.save
+      redirect_to ("/area")
+    end
   end
 
   private
 
   def area_params
-    params.require(:area).permit(:zipcode)
+    params.require(:area).permit(:zipcode, :prefcode, :address1, :address2, :address3, :kana1, :kana2, :kana3)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'areas/index'
-  get 'areas/new'
+  get "/" => "areas#index"
+  patch '/areas' => "areas#create"
+  get 'areas/search' => "areas#search_get"
   post 'areas/search' => "areas#search"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
